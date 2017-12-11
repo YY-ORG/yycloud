3bdb95e72e75236d564cd1202396e78faa01891a
XJ CHEN <xuejin.chen@gemii.cc>
2017-12-11 20:06:11 +0800

    Add Assess Paper Submit Support.

---
+++ yy-core/yy-assess/src/main/java/com/yy/cloud/core/assess/data/domain/PerAssessPaperExamineeMap.java
@@ -0,0 +1,92 @@
+package com.yy.cloud.core.assess.data.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the per_assess_paper_examinee_map database table.
+ * 
+ */
+@Entity
+@Table(name="per_assess_paper_examinee_map")
+@NamedQuery(name="PerAssessPaperExamineeMap.findAll", query="SELECT p FROM PerAssessPaperExamineeMap p")
+public class PerAssessPaperExamineeMap implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+	@Column(name="ID")
+	private String id;
+
+	@Column(name="ASSESS_PAPER_ID")
+	private String assessPaperId;
+
+	@Column(name="CREATE_DATE")
+	private Timestamp createDate;
+
+	@Column(name="CREATOR_ID")
+	private String creatorId;
+
+	private byte status;
+
+	@Column(name="UPDATE_DATE")
+	private Timestamp updateDate;
+
+	public PerAssessPaperExamineeMap() {
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAssessPaperId() {
+		return this.assessPaperId;
+	}
+
+	public void setAssessPaperId(String assessPaperId) {
+		this.assessPaperId = assessPaperId;
+	}
+
+	public Timestamp getCreateDate() {
+		return this.createDate;
+	}
+
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getCreatorId() {
+		return this.creatorId;
+	}
+
+	public void setCreatorId(String creatorId) {
+		this.creatorId = creatorId;
+	}
+
+	public byte getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(byte status) {
+		this.status = status;
+	}
+
+	public Timestamp getUpdateDate() {
+		return this.updateDate;
+	}
+
+	public void setUpdateDate(Timestamp updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
