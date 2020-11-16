79ca37c5a9a2c552650305d6c28a12e33717c4fd
XJ CHEN <xuejin.chen@gemii.cc>
2020-11-17 00:13:34 +0800

    Add Annual List.

--- yy-core/yy-assess/src/main/java/com/yy/cloud/core/assess/data/repositories/PerAssessAnswerRepository.java
+++ yy-core/yy-assess/src/main/java/com/yy/cloud/core/assess/data/repositories/PerAssessAnswerRepository.java
@@ -43,7 +43,7 @@ public interface PerAssessAnswerRepository extends JpaRepository<PerAssessAnswer
                                                                   @Param("assessCategoryId") String _assessCategoryId,
                                                                   @Param("userId") String _userId);
     @Query("select count(pa.id) as completedCount from PerAssessAnswer pa, PerAssessAspMap pam " +
-            " where pa.status=2 and pa.assessId = pam.assessId and pa.assessPaperId = pam.assessPaperId and pam.assessCategoryId=:assessCategoryId and pam.assessPaperId=:assessPaperId and pa.creatorId=:userId")
+            " where pa.status>=2 and pa.assessId = pam.assessId and pa.assessPaperId = pam.assessPaperId and pam.assessCategoryId=:assessCategoryId and pam.assessPaperId=:assessPaperId and pa.creatorId=:userId")
     PerAssessAnswerCount getCompletedAnswerCount(
             @Param("assessPaperId") String _assessPaperId,
             @Param("assessCategoryId") String _assessCategoryId,
